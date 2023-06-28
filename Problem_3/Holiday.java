45a233dbffe4a53cca937ee166ffc2a5a237cc68
ZazeNya <115577504+ZazeNya@users.noreply.github.com>
2023-06-28 17:05:49 +0300

    Create

---
+++ Problem_3/Holiday.java
@@ -0,0 +1,22 @@
+package study;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import static study.NewCalendar.*;
+public class Holiday {
+    public static DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+    public static List<Calendar> HOLIDAY_LIST = new ArrayList<Calendar>() {
+        {
+            this.add(new GregorianCalendar(Calendar.YEAR, Calendar.JANUARY, 1));
+            this.add(new GregorianCalendar(Calendar.YEAR, Calendar.JANUARY, 2));
+            this.add(new GregorianCalendar(Calendar.YEAR, Calendar.JANUARY, 7));
+            this.add(new GregorianCalendar(Calendar.YEAR, Calendar.JANUARY, 1));
+            this.add(new GregorianCalendar(Calendar.YEAR, Calendar.MAY, 1));
+            this.add(new GregorianCalendar(Calendar.YEAR, Calendar.MAY, 9));
+            this.add(new GregorianCalendar(Calendar.YEAR, Calendar.JULY, 3));
+            this.add(new GregorianCalendar(Calendar.YEAR, Calendar.NOVEMBER, 7));
+            this.add(new GregorianCalendar(Calendar.YEAR, Calendar.DECEMBER, 25));
+        }
+    };
+
+}
